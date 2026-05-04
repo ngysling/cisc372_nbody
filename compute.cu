3ba306e838c17f5e7afea250fca0025734b84911
ngysling <nate@gysli.ng>
2026-05-04 19:52:22 -0400

    seemingly working implementation, about 200ms gain, needs optimization

--- compute.cu
+++ compute.cu
@@ -2,6 +2,7 @@
 #include <math.h>
 #include "vector.h"
 #include "config.h"
+#include <cuda_runtime.h> 
 
 //compute: Updates the positions and locations of the objects in the system based on gravity.
 //Parameters: None
@@ -20,6 +21,23 @@ __global__ void compute(vector3* pos, vector3* vel, double* mass, vector3* matri
 		double magnitude=sqrt(magnitude_sq);
 		double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
 		FILL_VECTOR(matrix[i * NUMENTITIES + j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
+	}
+}
+
+__global__ void sumMatrix(vector3* hPos, vector3* hVel, vector3* matrix, int N) {
+	int i = blockIdx.x * blockDim.x + threadIdx.x; // i is our thread index.
+	if(i < N) {
+		vector3 accel_sum={0,0,0}; 
+		int row = i * N; 
+		for(int j = 0; j < N; j++) {
+			accel_sum[0] += matrix[row + j][0]; 
+			accel_sum[1] += matrix[row + j][1]; 
+			accel_sum[2] += matrix[row + j][2]; 
+		}
+		for (int k = 0; k<3; k++) {
+			hVel[i][k] += accel_sum[k] * INTERVAL; 
+			hPos[i][k] += accel_sum[k] * INTERVAL; 
+		}
 	}
 }
     
